b39320cf2cc67ac7845d0e668991277798dc5104
Julio <juliocezampieri@gmail.com>
2023-11-22 11:44:57 -0300

    botão logout

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,13 +1,20 @@
-import {} from "react";
-import "./style.css";
-
+import { useNavigate } from 'react-router-dom';
+import './style.css';
 
 const Header = () => {
+  const navigate = useNavigate();
+
+  const handleLogout = () => {
+    sessionStorage.removeItem('user');
+    navigate('/login');
+  };
+
   return (
     <header className="header">
-        <div><h1 className="header-title">MediSensi</h1></div>
+      <div><h1 className="header-title">MediSensi</h1></div>
       <div className="header-content">
         <div><h1 className="header-title">Global Solution 2023 - Engenharia de Software</h1></div>
+        <button onClick={handleLogout} className='button-off'>Logout</button>
       </div>
     </header>
   );
